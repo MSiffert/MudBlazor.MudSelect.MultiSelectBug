31db6d73a2021802adebdc46103c6163490e5a1f
Marco Siffert <marco.siffert@xappido.com>
2022-04-12 12:30:37 +0200

    Initial

---
+++ Country.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MudBlazor.MudSelect.Bug
+{
+    public class Country
+    {
+        public Guid Id { get; set; }
+        
+        public string Name { get; set; }
+    }
+}
